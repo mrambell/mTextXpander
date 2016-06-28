d00c3e0d7cb5b55af31999bdec13737e0fcd9538
Mattia Rambelli (TS-DE) <mattia_rambelli@de-macp-mara-9.local>
2016-06-28 10:05:09 +0200

    Initial Commit

---
+++ mTextXpander/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  mTextXpander
+//
+//  Created by Mattia Rambelli (TS-DE) on 28/06/16.
+//  Copyright © 2016 rms. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
